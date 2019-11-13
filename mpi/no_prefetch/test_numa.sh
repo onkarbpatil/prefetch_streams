9e106893e5ce4ad5a8073a553e3690183ffd6793
Onkar Patil <opatil@ncsu.edu>
2019-11-13 15:07:39 -0500

    modified:   mpi/no_prefetch/test_numa.sh 	modified:   mpi/test_numa.sh 	modified:   stream/no_prefetch/test_numa.sh 	modified:   stream/test_numa.sh 	new file:   test.sh

--- mpi/no_prefetch/test_numa.sh
+++ mpi/no_prefetch/test_numa.sh
@@ -4,11 +4,11 @@ make
 echo "#Strong scaling"
 thr=1
 export OMP_NUM_THREADS=1
-mpirun -np $thr ./numatest_mpi 17179869184
+mpirun -oversubscribe  --bind-by core -np $thr ./numatest_mpi 17179869184
 #cp sicm_numa_config sicm_numa_config_o1
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
-mpirun -np $thr	./numatest_mpi 17179869184
+mpirun -oversubscribe  --bind-by core -np $thr	./numatest_mpi 17179869184
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let thr=thr+24
 done
@@ -18,12 +18,12 @@ thr=1
 size=178956971
 size1=178956971
 export OMP_NUM_THREADS=1
-mpirun -np $thr ./numatest_mpi $size
+mpirun -oversubscribe  --bind-by core -np $thr ./numatest_mpi $size
 #cp sicm_numa_config sicm_numa_config_o1
 let size=size*24
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
-mpirun -np $thr	./numatest_mpi $size
+mpirun -oversubscribe  --bind-by core -np $thr	./numatest_mpi $size
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let size=size+24*size1
 	let thr=thr+24
---
+++ test.sh
@@ -0,0 +1,8 @@
+cd mpi
+sh test_numa.sh
+cd no_prefetch
+sh test_numa.sh
+cd ../../stream
+sh test_numa.sh
+cd no_prefetch
+sh test_numa.sh
