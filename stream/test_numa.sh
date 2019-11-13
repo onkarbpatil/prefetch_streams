373a4555588a85a93c7cc63b6765e2e1a7bed146
Onkar Patil <opatil@ncsu.edu>
2019-11-12 21:31:09 -0500

    new file:   stream/test_numa.sh 	modified:   test_numa.sh

--- stream/test_numa.sh
+++ stream/test_numa.sh
@@ -4,26 +4,30 @@ make
 echo "#Strong scaling"
 thr=1
 export OMP_NUM_THREADS=1
-mpirun -np $thr ./numatest_mpi 17179869184
+mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=2147483648 stream_mpi.c -O1 -lnuma -lm
+mpirun -np $thr ./stream_mpi 
 #cp sicm_numa_config sicm_numa_config_o1
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
-mpirun -np $thr	./numatest_mpi 17179869184
+mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=2147483648 stream_mpi.c -O1 -lnuma -lm
+mpirun -np $thr	./stream_mpi 
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let thr=thr+24
 done
 
 echo "#Weak scaling"
 thr=1
-size=178956971
-size1=178956971
+size=22369622
+size1=22369622
 export OMP_NUM_THREADS=1
-mpirun -np $thr ./numatest_mpi $size
+mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=$size stream_mpi.c -O1 -lnuma -lm
+mpirun -np $thr ./stream_mpi $size
 #cp sicm_numa_config sicm_numa_config_o1
 let size=size*24
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
-mpirun -np $thr	./numatest_mpi $size
+mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=$size stream_mpi.c -O1 -lnuma -lm
+mpirun -np $thr	./stream_mpi 
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let size=size+24*size1
 	let thr=thr+24
