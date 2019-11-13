b160482813433f69f66ce343f86a851e1faa6a02
Onkar Patil <opatil@ncsu.edu>
2019-11-13 15:12:59 -0500

    modified:   mpi/no_prefetch/test_numa.sh 	modified:   mpi/test_numa.sh 	modified:   stream/no_prefetch/test_numa.sh 	modified:   stream/test_numa.sh

--- stream/no_prefetch/test_numa.sh
+++ stream/no_prefetch/test_numa.sh
@@ -5,12 +5,12 @@ echo "#Strong scaling"
 thr=1
 export OMP_NUM_THREADS=1
 mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=2147483648 stream_mpi.c -O1 -lnuma -lm
-mpirun -oversubscribe  --bind-by core -np $thr ./stream_mpi 
+mpirun -oversubscribe  --bind-to core -np $thr ./stream_mpi 
 #cp sicm_numa_config sicm_numa_config_o1
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
 mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=2147483648 stream_mpi.c -O1 -lnuma -lm
-mpirun -oversubscribe  --bind-by core -np $thr	./stream_mpi 
+mpirun -oversubscribe  --bind-to core -np $thr	./stream_mpi 
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let thr=thr+24
 done
@@ -21,13 +21,13 @@ size=22369622
 size1=22369622
 export OMP_NUM_THREADS=1
 mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=$size stream_mpi.c -O1 -lnuma -lm
-mpirun -oversubscribe  --bind-by core -np $thr ./stream_mpi $size
+mpirun -oversubscribe  --bind-to core -np $thr ./stream_mpi $size
 #cp sicm_numa_config sicm_numa_config_o1
 let size=size*24
 while [ $thr -lt 97 ]; do
 #	export OMP_NUM_THREADS=$thr
 mpicc -o stream_mpi -DSTREAM_ARRAY_SIZE=$size stream_mpi.c -O1 -lnuma -lm
-mpirun -oversubscribe  --bind-by core -np $thr	./stream_mpi 
+mpirun -oversubscribe  --bind-to core -np $thr	./stream_mpi 
 #	cp sicm_numa_config "sicm_numa_config_o$thr"
 	let size=size+24*size1
 	let thr=thr+24
